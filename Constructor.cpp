a3f914e3299340908b1a2ff50c87e2bb624d9f4c
Anubhav Prashant Kharkar <anubhavprashantkharkar@Anubhavs-MacBook-Pro.local>
2026-01-15 15:08:20 +0530

    Copy constructor done

--- Constructor.cpp
+++ Constructor.cpp
@@ -3,29 +3,51 @@ using namespace std;
 
 class Customer
 {
-    public:
     string name;
     int account_number;
     int balance;
-    Customer()
+    public:
+    Customer()//Default constructor
     {
-        cout<<"COnstructor is called"<<endl;//Default constructor
+        name="Rohit";
+        account_number=3030;
+        balance=5000;
     }
-    Customer(string n,int ac,int b)
-    {
-        name=n;
-        account_number=ac;
-        balance=b;
-    }
+    // Customer(string n,int ac,int b)//Parameterized constructor
+    // {
+    //     name=n;
+    //     account_number=ac;
+    //     balance=b;
+    // }
     Customer(string name,int account_number,int balance)//this pointer, always points to the object
     {
         this->name=name;
         this->account_number=account_number;
         this->balance=balance;
     }
+
+    //Inline Constructor
+    // inline Customer(string a,int b, int c):name(a),account_number(b),balance(c){
+
+    // }
+    Customer(Customer &B)//copy constructor:please note the why behind this passing arguments by reference
+    {
+        name=B.name;
+        account_number=B.account_number;
+        balance=B.balance;
+
+    }
+    void display()
+    {
+        cout<<name<<" "<<account_number<<" "<<balance<<endl;
+    }
 };
 int main()
 {
     Customer one;
-    Customer two("Rohit",3030,5000);
+    one.display();
+    Customer two("Mohit",3031,5000);
+    two.display();
+    Customer three(two);
+    three.display();
 }
