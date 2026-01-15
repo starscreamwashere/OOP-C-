e848f26778200b6b58b2b6ccd01c277295968f5d
Anubhav Prashant Kharkar <anubhavprashantkharkar@Anubhavs-MacBook-Pro.local>
2026-01-15 12:47:58 +0530

    This pointer done

--- Constructor.cpp
+++ Constructor.cpp
@@ -11,8 +11,21 @@ class Customer
     {
         cout<<"COnstructor is called"<<endl;//Default constructor
     }
+    Customer(string n,int ac,int b)
+    {
+        name=n;
+        account_number=ac;
+        balance=b;
+    }
+    Customer(string name,int account_number,int balance)//this pointer, always points to the object
+    {
+        this->name=name;
+        this->account_number=account_number;
+        this->balance=balance;
+    }
 };
 int main()
 {
     Customer one;
+    Customer two("Rohit",3030,5000);
 }
